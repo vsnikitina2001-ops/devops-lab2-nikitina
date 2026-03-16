4a88a404ff3e8747db2f06c5afcb4ef29078e25d
vsnikitina2001-ops <vsnikitina2001@gmail.com>
2026-03-17 00:30:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+cat > app.py << 'EOF'
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Docker!"
+    return 'Hello, World! from CI/CD Pipeline'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+EOF
